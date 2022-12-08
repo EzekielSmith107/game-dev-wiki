bf8f557419b32ac376ee37310cf28580fafecc7e
EzekielSmith107 <ezesmith107@gmail.com>
2022-12-08 14:17:40 -0500

    added placeholder text

---
+++ src/pages/Creation.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+const Creation = () => {
+  return (
+    <div>
+      <h1>3DS Max</h1>
+      <p>
+        Autodesk 3ds Max, formerly 3D Studio and 3D Studio Max, is a
+        professional 3D computer graphics program for making 3D animations,
+        models, games and images. It is developed and produced by Autodesk Media
+        and Entertainment. It has modeling capabilities and a flexible plugin
+        architecture and must be used on the Microsoft Windows platform. It is
+        frequently used by video game developers, many TV commercial studios,
+        and architectural visualization studios. It is also used for movie
+        effects and movie pre-visualization. 3ds Max features shaders (such as
+        ambient occlusion and subsurface scattering), dynamic simulation,
+        particle systems, radiosity, normal map creation and rendering, global
+        illumination, a customizable user interface, and its own scripting
+        language.
+      </p>
+      <h1>Maya</h1>
+      <p>
+        Maya is an application used to generate 3D assets for use in film,
+        television, games, and commercials. The software was initially released
+        for the IRIX operating system. However, this support was discontinued in
+        August 2006 after the release of version 6.5. Maya was available in both
+        "Complete" and "Unlimited" editions until August 2008, when it was
+        turned into a single suite.[11] Users define a virtual workspace (scene)
+        to implement and edit media of a particular project. Scenes can be saved
+        in a variety of formats, the default being .mb (Maya D). Maya exposes a
+        node graph architecture. Scene elements are node-based, each node having
+        its own attributes and customization. As a result, the visual
+        representation of a scene is based entirely on a network of
+        interconnecting nodes, depending on each other's information. For the
+        convenience of viewing these networks, there is a dependency and a
+        directed acyclic graph. Nowadays, the 3D models can be imported to game
+        engines such as Unreal Engine and Unity.
+      </p>
+      <h1>Photoshop</h1>
+      <p>
+        Adobe Photoshop is a raster graphics editor developed and published by
+        Adobe Inc. for Windows and macOS. It was originally created in 1988 by
+        Thomas and John Knoll. Since then, the software has become the industry
+        standard not only in raster graphics editing, but in digital art as a
+        whole. The software's name is often colloquially used as a verb (e.g.
+        "to photoshop an image", "photoshopping", and "photoshop contest")[5]
+        although Adobe discourages such use.[6] Photoshop can edit and compose
+        raster images in multiple layers and supports masks, alpha compositing
+        and several color models including RGB, CMYK, CIELAB, spot color, and
+        duotone. Photoshop uses its own PSD and PSB file formats to support
+        these features. In addition to raster graphics, Photoshop has limited
+        abilities to edit or render text and vector graphics (especially through
+        clipping path for the latter), as well as 3D graphics and video. Its
+        feature set can be expanded by plug-ins; programs developed and
+        distributed independently of Photoshop that run inside it and offer new
+        or enhanced features.
+      </p>
+      <h1>Wwise</h1>
+      <p>
+        The Wwise authoring application uses a graphical interface to centralize
+        all aspects of audio creation. The functionality in this interface
+        allows sound designers to: Import audio files for use in video games
+        Apply audio plug-in effects Mix in real-time Define game states Simulate
+        audio environments Manage sound integration Apply the Windows Spatial
+        Audio API, or Dolby Atmos. Wwise allows for on-the-fly audio authoring
+        directly in game. Over a local network, users can create, audition, and
+        tweak sound effects and subtle sound behaviors while the game is being
+        played on another host. Wwise also includes the following components:
+        Cross-platform sound engine (Wwise Authoring) Multichannel Creator
+        (allows creation of multichannel audio) Plug-in architecture for source,
+        effect, and source control plug-ins, part of Wwise Launcher SoundFrame
+        API Wave Viewer (allows for sampling of WAV audio files)
+      </p>
+    </div>
+  );
+};
+
+export default Creation;
---
+++ src/pages/Perforce.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Perforce = () => {
+  return <div>Perforce</div>;
+};
+
+export default Perforce;
